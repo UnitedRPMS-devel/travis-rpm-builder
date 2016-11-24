cd94819b01f2932065680b87880964d3d6fbb0a6
Pavlo Rudyi <paulcarroty@riseup.net>
2016-11-24 18:16:06 +0200

    Add

---
+++ run-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+PACKAGE=$(grep packages_buildrequires_git .travis.yml | cut -d '/' -f2)
+DISTRO=$(grep mock_config .travis.yml | cut -d ' ' -f4)
+DATE=$(date +"%Y%m%d:%H:%m")
+
+git fetch --tags
+
+git add . && git commit -m "Add `$PACKAGE`"
+
+git tag -a $PACKAGE${DISTRO^^}$DATE -m $PACKAGE${DISTRO^^}
+
+git push --tags origin master
