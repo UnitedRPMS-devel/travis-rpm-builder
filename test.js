2d813355f24ed015a37a6589194978a4b1628491
Pavlo Rudyi <paulcarroty@riseup.net>
2017-04-29 23:13:50 +0300

    libva-intel-driver

--- test.js
+++ test.js
@@ -22,11 +22,11 @@ var distro = BuildConfig.split('\n')[1].split('-')[1].replace('rawhide','27');
 const gitAdd = spawn( 'git', [ 'add', '.' ]);
 if (~gitAdd.stderr) { console.log(gitAdd.stderr.toString());  }
 
-const gitCommit = spawn('git', ['commit', '-m', package]);
+const gitCommit = spawn('/usr/bin/git', ['commit', '-m', package]);
 if (~gitCommit.stderr) { console.log(gitCommit.stderr.toString());  }
 
-const gitTag = spawn('git', ['tag', '-a', package + '-F' + distro +'-'+ datestr, '-m', package + distro]);
+const gitTag = spawn('/usr/bin/git', ['tag', '-a', package + '-F' + distro +'-'+ datestr, '-m', package + distro]);
 if (~gitTag.stderr) { console.log(gitTag.stderr.toString());  }
 
-const gitPush = spawn('git', ['push', '--tags', 'origin', 'master']);
+const gitPush = spawn('/usr/bin/git', ['push', '--tags', 'origin', 'master']);
 // if (~gitPush.stderr) { console.log(gitPush.stderr.toString());  }
